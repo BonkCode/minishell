58da4934242cfb1d2bfdb3c7ae13c16786194bd2
Ilia Petrov <idzaoshang@yandex.ru>
2020-09-26 17:17:35 +0300

    fixed for govnorewiew

--- commands/pwd/pwd.c
+++ commands/pwd/pwd.c
@@ -1,20 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 15:37:27 by rvernius          #+#    #+#             */
-/*   Updated: 2020/09/26 16:17:14 by rvernius         ###   ########.fr       */
+/*   Updated: 2020/09/26 17:16:09 by rvernius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <
 
-int ft_pwd()
+int ft_pwd(t)
 {
 	char *buf;
 
@@ -22,3 +23,4 @@ int ft_pwd()
 	printf("%s\n", buf);
 	return (0);
 }
+
