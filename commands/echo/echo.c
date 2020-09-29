7b33f99b3a6b734b1c6ab0d351b42720db37ee40
Ilia Petrov <idzaoshang@yandex.ru>
2020-09-29 14:02:03 +0300

    echoed

--- commands/echo/echo.c
+++ commands/echo/echo.c
@@ -1,22 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/26 15:37:27 by rvernius          #+#    #+#             */
-/*   Updated: 2020/09/29 13:28:17 by rvernius         ###   ########.fr       */
+/*   Created: 2020/09/29 13:26:29 by rvernius          #+#    #+#             */
+/*   Updated: 2020/09/29 14:00:51 by rvernius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/commands.h"
 
-int		ft_pwd(t_command command)
+int		ft_echo(t_command command)
 {
-	char *buf;
+	int i;
+	int n;
 
-	buf = getcwd(NULL, 0);
-	printf("%s\n", buf);
+	n = 0;
+	i = 0;
+	if(ft_strcmp(command.command->flags, "-n") == 0)
+		n = 1;
+	if (!command.command->arguments[0])
+	{
+		if (!n)
+			ft_putstr_fd("\n", 1);
+		return (0);
+	}
+	while (command.command->arguments[i])
+	{
+		ft_putstr_fd(command.command->arguments[i], 1);
+		command.command->arguments[i + 1] ? ft_putstr_fd(" ", 1) : 0;
+		++i;
+	}
+	!n ? ft_putstr_fd("\n", 1) : 0;
 	return (0);
 }
--- headers/commands.h
+++ headers/commands.h
@@ -6,11 +6,16 @@
 /*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 16:17:59 by rvernius          #+#    #+#             */
-/*   Updated: 2020/09/26 16:19:06 by rvernius         ###   ########.fr       */
+/*   Updated: 2020/09/29 13:26:23 by rvernius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMANDS_H
 # define COMMANDS_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "m_types.h"
 
 #endif
