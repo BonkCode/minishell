82e5b2e227017a5a3b712f66ccba40bd1954d86d
Ilia Petrov <idzaoshang@yandex.ru>
2020-09-29 13:23:04 +0300

    fixed

--- srcs/commands/echo/ft_echo.c
+++ srcs/commands/echo/ft_echo.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/30 20:06:55 by rtrant            #+#    #+#             */
-/*   Updated: 2020/09/26 17:35:03 by rvernius         ###   ########.fr       */
+/*   Created: 2020/09/26 17:36:11 by rvernius          #+#    #+#             */
+/*   Updated: 2020/09/26 17:38:21 by rvernius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../../libft/libft.h"
+#include "../../../headers/m_types.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t num)
+int	ft_echo(t_command command)
 {
-	size_t i;
-
-	i = 0;
-	if (!s1 || !s2)
-		return (-1);
-	while (s1[i] && i < num)
-	{
-		if (s1[i] != s2[i])
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
-		i++;
-	}
-	if (s2[i] == '\0' || i == num)
-		return (0);
-	else
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	ft_putstr_fd(command.arguments[0]);
+	return (0)
 }
