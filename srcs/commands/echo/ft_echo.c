ac7bce3e316c7abf9233a60cdb29bdd7049513d1
Ilia Petrov <idzaoshang@yandex.ru>
2020-10-08 17:22:53 +0300

    a

--- srcs/commands/echo/ft_echo.c
+++ srcs/commands/echo/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 13:26:29 by rvernius          #+#    #+#             */
-/*   Updated: 2020/10/01 15:50:32 by rvernius         ###   ########.fr       */
+/*   Updated: 2020/10/08 17:22:08 by rvernius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ void		ft_echo(t_command command)
 	i = 0;
 	if(ft_strcmp(command.command->flags, "-n") == 0)
 		n = 1;
-	if (!command.command->arguments[0])
+	if (!command.command->args[0])
 	{
 		if (!n)
 			ft_putstr_fd("\n", 1);
 		exit(0);
 	}
-	while (command.command->arguments[i])
+	while (command.command->args[i])
 	{
-		ft_putstr_fd(command.command->arguments[i], 1);
-		command.command->arguments[i + 1] ? ft_putstr_fd(" ", 1) : 0;
+		ft_putstr_fd(command.command->args[i], 1);
+		command.command->args[i + 1] ? ft_putstr_fd(" ", 1) : 0;
 		++i;
 	}
 	!n ? ft_putstr_fd("\n", 1) : 0;
