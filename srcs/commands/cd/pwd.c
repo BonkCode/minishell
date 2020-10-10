16c7b3d31a384fdca917bdec013fe8c9222287ce
Ilia Petrov <idzaoshang@yandex.ru>
2020-10-10 17:22:21 +0300

    pwd fixed

--- srcs/commands/cd/pwd.c
+++ srcs/commands/cd/pwd.c
@@ -6,17 +6,33 @@
 /*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 15:37:27 by rvernius          #+#    #+#             */
-/*   Updated: 2020/10/01 12:52:30 by rvernius         ###   ########.fr       */
+/*   Updated: 2020/10/10 17:21:44 by rvernius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../headers/commands.h"
 
-int		ft_pwd(t_command command)
+void	ft_pwd(t_command command)
 {
-	char *buf;
+	char		*buf;
+	int			i;
 
-	buf = getcwd(NULL, 0);
-	ft_putendl_fd(buf, 1);
-	return (0);
+	i = 0;
+	while (command.commands->args)
+	{
+		++i;
+		command.commands->args = command.commands->args->next;
+	}
+	--i;
+	if (i > 0)
+	{
+		ft_putendl_fd("pwd: too many arguments", 2);
+		exit(1);
+	}
+	else
+	{
+		buf = getcwd(NULL, 0);
+		ft_putendl_fd(buf, 1);
+	}
+	exit(0);
 }
