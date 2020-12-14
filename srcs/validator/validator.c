5db402a82455183e363835886f42010855e845b2
Bonk <aoliounin12@gmail.com>
2020-12-14 21:48:02 +0300

    added double redirection error check

--- srcs/validator/validator.c
+++ srcs/validator/validator.c
@@ -6,7 +6,7 @@
 /*   By: rtrant <rtrant@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 13:44:55 by rtrant            #+#    #+#             */
-/*   Updated: 2020/12/12 16:36:03 by rtrant           ###   ########.fr       */
+/*   Updated: 2020/12/14 21:47:46 by rtrant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,10 @@ int	validate_tokens(char **tokens)
 {
 	int	i;
 	int	len;
+	int	redirected;
 
 	i = 0;
+	redirected = 0;
 	len = get_2d_arr_len(tokens);
 	if (!ft_strncmp(tokens[0], ";", 2))
 		return ((0<<8) + STAT_SYNTAX_ERR);
@@ -45,6 +47,12 @@ int	validate_tokens(char **tokens)
 			return ((i<<8) + STAT_SYNTAX_ERR);
 		else if (!ft_strncmp_split(tokens[i], "< > >>", ' ') && tokens[i + 3] && !ft_strncmp(tokens[i + 4], "|", 2))
 			return ((i<<8) + STAT_DOUBLE_REDIRECTION_ERR);
+		if (!ft_strncmp_split(tokens[i], "< > >>", ' '))
+			redirected = 1;
+		if (!ft_strncmp_split(tokens[i], "< > >> |", ' ') && redirected)
+			return ((i<<8) + STAT_DOUBLE_REDIRECTION_ERR);
+		if (!ft_strncmp(tokens[i], ";", 2))
+			redirected = 0;
 		i += 2;
 	}
 	return (STAT_OK);
